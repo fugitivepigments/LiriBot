032de3743fd472ca593d84df3a047ea1be5f6cf7
Case Western Reserve University <andrew.mancuso@case.edu>
2018-04-06 21:00:48 -0400

    updated js began debugging

--- liriBot.js
+++ liriBot.js
@@ -3,26 +3,13 @@ require("dotenv").config();
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var inquirer = require('inquirer');
+var fs = require('fs');
 var omdb = require('omdb');
 var request = require('request');
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
-// var input = (process.argv[2]);
-//
-// if (input === 'my-tweets') {
-//   var params = {
-//     screen_name: 'rustyferrari',
-//     count: 20
-//   };
-//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//     if (!error) {
-//       console.log(JSON.stringify(tweets, null, 2));
-//     }
-//   });
-// };
 
 //prompting user what they want to do
 inquirer.prompt([{
@@ -37,11 +24,13 @@ inquirer.prompt([{
     var params = {
       screen_name: 'rustyferrari',
       count: 20,
-      trim_user: true,
     };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
-        console.log(JSON.stringify(tweets, null, 2));
+        for (var i = 0; i < tweets.length; i++) {
+          console.log(tweets[i].created_at);
+          console.log(tweets[i].text);
+        }
       }
     });
   }
@@ -60,7 +49,11 @@ inquirer.prompt([{
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-        console.log(data);
+        console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
+
       })
     })
   }
@@ -73,37 +66,46 @@ inquirer.prompt([{
       //then search for movie...
     }]).then(function(inquirerResponse) {
       var queryUrl = "http://www.omdbapi.com/?t=" + inquirerResponse.whatMovie + "&y=&plot=short&apikey=trilogy";
-      omdb.search(queryUrl, function(err, movies) {
-
-        if (err) {
-          return console.error(err);
+      var mrNobody = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy";
+      request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+          console.log('Title: ' + JSON.parse(body).Title);
+          console.log('This film was made in: ' + JSON.parse(body).Year);
+          console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
+          console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value);
+          console.log('Production Country: ' + JSON.parse(body).Country);
+          console.log('Language: ' + JSON.parse(body).Language);
+          console.log('Plot: ' + JSON.parse(body).Plot);
+          console.log('Actors: ' + JSON.parse(body).Actors);
         }
-
-        if (movies.length < 1) {
-          return console.log('No movies found!');
-        }
-
-        movies.forEach(function(movie) {
-          console.log('%s (%d)', movie.title, movie.year);
-        });
-
-
       });
+    })
+  }
+  //if response is do what it says...
+  if (inquirerResponse.whatToDo === 'Do What It Says') {
+    inquirer.prompt([{
+      type: 'confirm',
+      message: 'Are you SURE?',
+      name: 'confirm',
+      default: true,
+    }]).then(function(inquirerResponse) {
+      var content;
+      fs.readFile('random.txt', 'utf8', function(err, data) {
+        content = data;
+      });
+      spotify.search({
+        type: 'track',
+        query: content,
+      }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log(JSON.stringify(data.tracks.items[0].name, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].external_urls.spotify, null, 2));
+        console.log(JSON.stringify(data.tracks.items[0].album.name, null, 2));
 
-      // omdb.get({
-      //   title: inquirerResponse.whatMovie
-      // }, function(err, movie) {
-      //   if (err) {
-      //     return console.error(err);
-      //   }
-      //
-      //   if (!movie) {
-      //     return console.log('Movie not found!');
-      //   }
-      //
-      //   console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-      //   console.log(movie.plot);
-      // })
+      })
     })
   }
 });
